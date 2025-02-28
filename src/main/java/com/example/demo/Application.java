a11b1f60644e3fe3ee7e4746b6d046aafc26ca56
Giancarlo.Hernandez <giancarlo.hernandez@homecredit.ph>
2025-02-28 10:51:00 +0800

    start of the new world

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello World XD");
 		SpringApplication.run(Application.class, args);
 	}
 
